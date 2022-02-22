bdd9d22cecb59582b5cf7e2f4859f4dcb17488a3
Caio_Olivieri <novnew05@gmail.com>
2022-02-22 20:19:24 -0300

    fixing

--- src/components/HomeHeros/styles.ts
+++ src/components/HomeHeros/styles.ts
@@ -90,7 +90,7 @@ export const CodeItem = styled.pre`
   font-family: 'JetBrains Mono', monospace;
   font-weight: 300;
   color: #fff;
-  width: 55rem;
+  width: 45rem;
   align-self: flex-start;
   transition: 1s;
 
