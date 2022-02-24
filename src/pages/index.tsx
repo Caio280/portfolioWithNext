4d1f248b07601c34e7ef71728a51149a72efcfcd
Caio_Olivieri <novnew05@gmail.com>
2022-02-24 15:50:10 -0300

    adding Footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import Header from '../components/Header';
 import HomeHeros from '../components/HomeHeros';
 import Experience from '../components/Experience';
 import Skills from '../components/Skills';
+import Footer from '../components/Footer';
 
 export default function Home() {
   return (
@@ -15,6 +16,8 @@ export default function Home() {
         <Experience />
         <Skills />
       </main>
+
+      <Footer />
     </HomeContainer>
   );
 }
